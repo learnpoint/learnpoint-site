e6b79a9da94bbb395d9106f90074cc99d600bdbb
Karl West <mickel.west@gmail.com>
2020-09-17 21:53:55 +0200

    404 page and dep upgrade

--- deps.js
+++ deps.js
@@ -1,2 +1,2 @@
 export * as path from "https://deno.land/std@0.69.0/path/mod.ts";
-export { sideServe, sideBuild } from "https://cdn.jsdelivr.net/gh/ekmwest/side@1.0.8/mod.js";
+export { sideServe, sideBuild } from "https://cdn.jsdelivr.net/gh/ekmwest/side@1.0.9/mod.js";
