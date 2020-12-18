a2148d5a3d62df27285962a6d2fb04b8b0e4410f
Karl West <mickel.west@gmail.com>
2020-12-18 22:27:32 +0100

    upgrade deps

--- deps.js
+++ deps.js
@@ -1,2 +1,2 @@
 export * as path from "https://deno.land/std@0.81.0/path/mod.ts";
-export { sideServe, sideBuild } from "https://cdn.jsdelivr.net/gh/ekmwest/side@1.1.3/mod.js";
+export { sideServe, sideBuild } from "https://cdn.jsdelivr.net/gh/ekmwest/side@3.0.0/mod.js";
