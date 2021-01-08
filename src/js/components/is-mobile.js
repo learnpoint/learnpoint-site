c6e82ca333a277673701dbfe0ebbd4898df3c1ee
Karl West <mickel.west@gmail.com>
2021-01-08 10:36:19 +0100

    initial is-mobile without delay

--- src/js/components/is-mobile.js
+++ src/js/components/is-mobile.js
@@ -1,9 +1,21 @@
 (function () {
     const MOBILE_WIDTH = 768;
 
+    function updateIsMobile() {
+        if (document.documentElement.clientWidth && document.documentElement.clientWidth <= MOBILE_WIDTH) {
+            window.isMobile = true;
+            document.documentElement.classList.add('is-mobile');
+        } else {
+            window.isMobile = false;
+            document.documentElement.classList.remove('is-mobile');
+        }
+    }
+
+    updateIsMobile();
+
     let timeout = null;
 
-    function updateIsMobile() {
+    window.addEventListener('resize', () => {
         const wait = 100; // 100ms debounce
 
         if (timeout) {
@@ -12,17 +24,8 @@
 
         timeout = setTimeout(() => {
             timeout = null;
-            if (document.documentElement.clientWidth && document.documentElement.clientWidth <= MOBILE_WIDTH) {
-                window.isMobile = true;
-                document.documentElement.classList.add('is-mobile');
-            } else {
-                window.isMobile = false;
-                document.documentElement.classList.remove('is-mobile');
-            }
+            updateIsMobile();
         }, wait);
-    }
+    });
 
-    window.addEventListener('resize', updateIsMobile);
-
-    updateIsMobile();
 })();
