ef06e45c9d1005b20d5ceb783ce25bb23c51bd3d
Karl West <mickel.west@gmail.com>
2025-02-21 18:29:08 +0100

    upgrade deno and piko

--- dev.js
+++ dev.js
@@ -1,4 +1,4 @@
-import { dev } from "https://cdn.jsdelivr.net/gh/learnpoint/piko@2.2.19/dev.js";
+import { dev } from "https://cdn.jsdelivr.net/gh/learnpoint/piko@3.0.0/dev.js";
 
 if (Deno.args.length > 0) {
     const port =  Deno.args[0];
